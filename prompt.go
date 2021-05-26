716706c93f4f7ac3385a698d3a4e2032cdb337f2
leep-frog <gleeper49@gmail.com>
2021-05-26 12:35:13 -0400

    update prompt

--- prompt.go
+++ prompt.go
@@ -3,41 +3,26 @@ package command
 import (
 	"bufio"
 	"os"
-	"sort"
 )
 
-func PromptUser(output Output, question string, answerActions map[string]func() error) error {
-	p := &Prompt{
-		Question:      question,
-		AnswerActions: answerActions,
-	}
-	return p.Prompt(output)
-}
-
 type Prompt struct {
-	Question      string
-	AnswerActions map[string]func() error
+	Question string
+	Chan     chan string
 }
 
-func (p *Prompt) Prompt(output Output) error {
+func (p *Prompt) Prompt(output Output) {
 	reader := bufio.NewReader(os.Stdin)
 	output.Stdout(p.Question)
 
-	for {
-		output.Stdout(": ")
-		text, err := reader.ReadString('\n')
-		if err != nil {
-			return err
+	go func() {
+		for {
+			output.Stdout(": ")
+			text, err := reader.ReadString('\n')
+			if err != nil {
+				output.Stderr("failed to read prompt input (%v); trying again", err)
+				continue
+			}
+			p.Chan <- text
 		}
-
-		if f, ok := p.AnswerActions[text]; ok {
-			return f()
-		}
-		var keys []string
-		for k := range p.AnswerActions {
-			keys = append(keys, k)
-		}
-		sort.Strings(keys)
-		output.Stdout("Response must be one of %v", keys)
-	}
+	}()
 }
