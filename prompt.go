cf2f64ea6d3d064ccc5b7146d82b135dd1c6ef10
leep-frog <gleeper49@gmail.com>
2021-05-26 15:32:38 -0400

    simpler prompt

--- prompt.go
+++ prompt.go
@@ -5,26 +5,21 @@ import (
 	"os"
 )
 
-type Prompt struct {
-	Question string
-	Chan     chan string
-}
-
-func (p *Prompt) Prompt(output Output) <-chan string {
+func Prompt(output Output, question string) chan string {
 	reader := bufio.NewReader(os.Stdin)
-	output.Stdout(p.Question)
+	output.Stdout(question)
+	c := make(chan string)
 
 	go func() {
 		for {
 			text, err := reader.ReadString('\n')
 			if err == nil {
-				p.Chan <- text
+				c <- text
 				return
 			}
 			output.Stderr("failed to read prompt input (%v); trying again", err)
-			continue
 		}
 	}()
 
-	return p.Chan
+	return c
 }
