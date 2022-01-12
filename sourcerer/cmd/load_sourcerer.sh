6896225f2791be5b510e80a093b1b1ad969571a4
leep-frog <gleeper49@gmail.com>
2022-01-12 16:01:59 -0500

    add gg function

--- sourcerer/cmd/load_sourcerer.sh
+++ sourcerer/cmd/load_sourcerer.sh
@@ -5,3 +5,16 @@ cd "$(dirname -- "${BASH_SOURCE[0]}")/sourcerer"
 tmpFile="$(mktemp)"
 go run *.go sourcerer > $tmpFile && source $tmpFile
 popd > /dev/null
+
+function gg {
+  for package in "$@"
+  do
+    if [ "$GO111MODULE" == "on" ]; then
+      commitSha="$(git ls-remote git@github.com:leep-frog/${package}.git | grep ma[is][nt] | awk '{print $1}')"
+      go get -v "github.com/leep-frog/$package@$commitSha"
+    else
+      go get -u "github.com/leep-frog/$package"
+    fi
+  done
+}
+
