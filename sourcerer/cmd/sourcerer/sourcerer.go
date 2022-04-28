0a1310f73cbe5af414e46c7f6b0288f005f11e95
leep-frog <gleeper49@gmail.com>
2022-04-27 20:56:12 -0400

    require at least one gg arg

--- sourcerer/cmd/sourcerer/sourcerer.go
+++ sourcerer/cmd/sourcerer/sourcerer.go
@@ -25,7 +25,7 @@ func (*UpdateLeepPackageCommand) Node() *command.Node {
 	pkg := "PACKAGE"
 	return command.SerialNodes(
 		command.Description("gg updates go packages from the github.com/leep-frog repository"),
-		command.ListArg[string](pkg, "Package name", 0, command.UnboundedList),
+		command.ListArg[string](pkg, "Package name", 1, command.UnboundedList),
 		command.ExecutableNode(func(o command.Output, d *command.Data) ([]string, error) {
 			var r []string
 			for _, p := range d.StringList(pkg) {
