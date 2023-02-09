3c942f13638d03f7ca418a34d63d5e5d7b1e78f0
leep-frog <gleeper49@gmail.com>
2023-02-09 11:56:57 -0500

    wrap load_sourcerer logic in function

--- sourcerer/cmd/load_sourcerer.sh
+++ sourcerer/cmd/load_sourcerer.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-pushd . > /dev/null
-cd "$(dirname -- "${BASH_SOURCE[0]}")/sourcerer"
-tmpFile="$(mktemp)"
-go run . sourcerer > $tmpFile && source $tmpFile
-popd > /dev/null
+# Need to wrap in a function so the `source $tmpFile`
+# command is run in a function (for local variables).
+function _initial_load() {
+  pushd . > /dev/null
+  cd "$(dirname -- "${BASH_SOURCE[0]}")/sourcerer"
+  tmpFile="$(mktemp)"
+  go run . sourcerer > $tmpFile && source $tmpFile
+  popd > /dev/null
+}
+_initial_load
 
 alias u=mancli
